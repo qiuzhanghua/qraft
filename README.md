609a8ba366e67aa9a923e2ab8e90ed8f73f78b67
qiuzhanghua <qiuzhanghua@icloud.com>
2022-08-29 09:34:08 +0800

    Add test for MySQL

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# Rust + Redis Client
+# Rust + Redis Client + MySQL
 
+## Test Redis
 ```bash
-cargo build --release
-target/release/qraft
+cargo run --release
 ```
+with m1x
 ```text
 ❯ wrk -t12 -c200 -d30s http://localhost:8080/
 Running 30s test @ http://localhost:8080/
@@ -16,13 +17,10 @@ Requests/sec:  55712.09
 Transfer/sec:      4.25MB
 ```
 
-with my m1x
-
-
 ```bash
 cargo run
 ```
-
+with old mac
 ```text
 ❯ wrk -t12 -c200 -d30s http://localhost:8080/
 Running 30s test @ http://localhost:8080/
@@ -33,4 +31,39 @@ Running 30s test @ http://localhost:8080/
   797191 requests in 30.02s, 60.82MB read
 Requests/sec:  26558.54
 Transfer/sec:      2.03MB
+```
+
+## Test MySQL
+```bash
+cargo run
+```
+with m1x
+
+```text
+❯ wrk -t12 -c200 -d30s http://localhost:8080/db
+Running 30s test @ http://localhost:8080/db
+  12 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency    15.27ms  441.17us  28.76ms   76.80%
+    Req/Sec     1.05k    30.68     1.13k    69.81%
+  377284 requests in 30.04s, 29.14MB read
+Requests/sec:  12560.64
+Transfer/sec:      0.97MB
+```
+
+```bash
+cargo run --release
+```
+m1x with release
+
+```text
+❯ wrk -t12 -c200 -d30s http://localhost:8080/db
+Running 30s test @ http://localhost:8080/db
+  12 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     5.64ms  399.45us  21.05ms   97.37%
+    Req/Sec     2.85k   106.35     2.94k    95.81%
+  1022376 requests in 30.01s, 78.98MB read
+Requests/sec:  34062.93
+Transfer/sec:      2.63MB
 ```
--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,30 @@ extern crate lazy_static;
 // extern crate actix_web;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-// use rand::prelude::*;
 use actix_web::web::Data;
 use dotenv::dotenv;
 use redis::cluster::ClusterClient;
 use redis::Commands;
-// use sqlx::any::{AnyPool, AnyPoolOptions};
 use std::env;
+use sqlx::any::AnyPoolOptions;
+use sqlx::{Any, Pool};
 
 #[get("/")]
-async fn hello(redis: Data<r2d2::Pool<ClusterClient>>) -> impl Responder {
-    let mut conn = redis.get().unwrap();
+async fn hello(redis_pool: Data<r2d2::Pool<ClusterClient>>) -> impl Responder {
+    let mut conn = redis_pool.get().unwrap();
     let s: String = conn.get("hello").unwrap();
     HttpResponse::Ok().body(s)
 }
+
+#[get("/db")]
+async fn db(db_pool: Data<Pool<Any>>) -> impl Responder {
+    let row: (String,) = sqlx::query_as(r#"SELECT version() v"#)
+        .fetch_one(&**db_pool)
+        .await.unwrap();
+    let s = row.0;
+    HttpResponse::Ok().body(s)
+}
+
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
@@ -43,16 +53,11 @@ lazy_static! {
 async fn main() -> Result<(), sqlx::Error> {
     dotenv().ok();
     env_logger::init();
-    // let pool = AnyPoolOptions::default()
-    //     .max_connections(5)
-    //     .connect(&DATABASE_URL)
-    //     .await?;
-    // println!("{:?}", pool.any_kind());
-    // let row: (String,) = sqlx::query_as(r#"SELECT version() v"#)
-    //     .fetch_one(&pool)
-    //     .await?;
-    // let ver = row.0;
-    // println!("{:?}", ver);
+    let db_pool: Pool<Any> = AnyPoolOptions::default()
+        .max_connections(5)
+        .connect(&DATABASE_URL)
+        .await?;
+
     let nodes = vec![
         "redis://127.0.0.1:7001/",
         "redis://127.0.0.1:7002/",
@@ -61,19 +66,16 @@ async fn main() -> Result<(), sqlx::Error> {
         "redis://127.0.0.1:7005/",
         "redis://127.0.0.1:7006/",
     ];
-    let client = ClusterClient::open(nodes).expect("redis not ready");
+    let redis_client = ClusterClient::open(nodes).expect("redis not ready");
 
-    let pool = r2d2::Pool::builder().build(client).unwrap();
+    let redis_pool = r2d2::Pool::builder().build(redis_client).unwrap();
 
-    // let mut conn = rdb::REDIS_CLUSTER
-    //     .get_connection()
-    //     .expect("redis not connected");
-    // let s: String = conn.get("hello").unwrap();
-    // println!("{}", s);
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(pool.clone()))
+            .app_data(Data::new(db_pool.clone()))
+            .app_data(Data::new(redis_pool.clone()))
             .service(hello)
+            .service(db)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
